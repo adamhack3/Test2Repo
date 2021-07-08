02ccd2c225083451aaa5dec045b50c640879bb37
adamhack3 <adam.tracy@hackney.gov.uk>
2021-07-08 10:11:21 +0100

    Testing Implemented

--- test_output_postcode_list.py
+++ test_output_postcode_list.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import re
+import pytest
 
 def get_addresses_for_postcode(postcode):
 
@@ -13,7 +14,7 @@ def get_addresses_for_postcode(postcode):
     # Verify valid postcode
     if match == None:
         print("Invalid postcode.\nPostcode must have a space e.g. E8 5ER")
-        return
+        return None
 
     base_url = f"https://6kb2p9kgb0.execute-api.eu-west-2.amazonaws.com/production/api/v1/addresses/?postcode={postcode}"
 
@@ -22,6 +23,8 @@ def get_addresses_for_postcode(postcode):
     }
 
     response = requests.get(base_url, headers=headers)
+
+    assert response.status_code == 200
 
     response = response.json()
 
@@ -36,14 +39,14 @@ def make_list_of_postcodes(json_dict):
         output = f'{address["line1"]}, {address["line2"]}, {address["line3"]}, {address["line4"]}, {address["town"]}, {address["postcode"]}, UPRN : {address["UPRN"]}'
         print(output)
 
-
-#test_postcodes = ["E5 8TE", "E5 8TE", "E8 2LN", "E8 1HH", "E8 2HH"]
-#for postcode in test_postcodes:
-#    get_addresses_for_postcode(postcode)
-
 # Ensure user friendly
 def main(postcode):
-    response = get_addresses_for_postcode(postcode)
+    response = test_get_addresses_for_postcode(postcode)
     make_list_of_postcodes(response)
+    return response
 
-main("E5 8TE")
+def test_main():
+    my_string 
+    assert main("E8 3LH") != None
+
+
